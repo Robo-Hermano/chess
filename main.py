9cf2dda7f0187aac49f6bbe232039b2533e862fb
Robo-Hermano <138218435+Robo-Hermano@users.noreply.github.com>
2024-12-01 11:15:36 +0000

    Update main.py

--- main.py
+++ main.py
@@ -12,9 +12,6 @@ letterToNumber = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8
 numberToLetter = {1: "a", 2: "b", 3: "c", 4: "d", 5: "e", 6: "f", 7: "g", 8: "h"}
 
 class Piece:
-  def __init__(self, image):
-    self.image = image
-    
   def get_captured(self):
     self.position = (-1,-1)
     
@@ -25,9 +22,10 @@ class Piece:
     return self.position
 
 class Pawn(Piece):
-  def __init__(self, position):
+  def __init__(self, position, image):
     self.val = 1
     self.position = position
+    self.image = image
 
   def movement(self, position):
     pass #slightly more complicated
@@ -39,45 +37,50 @@ class Pawn(Piece):
     pass
     
 class Knight(Piece):
-  def __init__(self, position):
+  def __init__(self, position, image):
     self.val = 3
     self.position = position 
+    self.image = image
 
   def movement(self, position):
     if abs(position[0] - self.position[0]) == 2 and abs(position[1] - self.position[1]) == 1 or abs(position[1] - self.position[1]) == 2 and abs(position[0] - self.position[0]) == 1:
       self.position = position
     
 class Bishop(Piece):
-  def __init__(self, position):
+  def __init__(self, position, image):
     self.val = 3
     self.position = position
+    self.image = image
 
   def movement(self, position):
     if abs(position[0] - self.position[0]) == abs(position[1] - self.position[1]):
       self.position = position
 
 class Rook(Piece):
-  def __init__(self, position):
+  def __init__(self, position, image):
     self.val = 5
     self.position = position
+    self.image = image
 
   def movement(self, position):
     if position[0] == self.position[0] or position[1] == self.position[1]:
       self.position = position
 
 class Queen(Piece):
-  def __init__(self, position):
+  def __init__(self, position, image):
     self.val = 9
     self.position = position
+    self.image = image
 
   def movement(self, position):
     if abs(position[0] - self.position[0]) == abs(position[1] - self.position[1]) or position[0] == self.position[0] or position[1] == self.position[1]:
       self.position = position
       
 class King(Piece):
-  def __init__(self, position):
+  def __init__(self, position, image):
     self.val = 0
     self.position = position
+    self.image = image
 
   def movement(self, position):
     if abs(self.position[0] - position[0]) <= 1 and abs(self.position[1] - position[1]) <= 1 and self.check_for_checks(position) == True:
