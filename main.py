4bbe27f4677a94a3de846d53c97327aec29076ea
Robo-Hermano <138218435+Robo-Hermano@users.noreply.github.com>
2024-12-08 09:18:32 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 import pygame
 
-def create_chess_position_board():
-  chessBoard = []
-  for i in range(8, 0):
-    for j in range(1, 9):
-      chessBoard.append((j, i))
-  return chessBoard
-
-chessBoard = create_chess_position_board()
-letterToNumber = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8}
-numberToLetter = {1: "a", 2: "b", 3: "c", 4: "d", 5: "e", 6: "f", 7: "g", 8: "h"}
-
 class Piece:
   def get_captured(self):
     self.position = (-1,-1) #so that the piece won't get printed after getting captured
